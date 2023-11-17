c16ee7084eca6a5880b5f9698794934a35526391
Luis Miguel <lmavendano1597@gmail.com>
2023-11-16 22:54:36 -0500

    next commit

--- src/app.js
+++ src/app.js
@@ -1,68 +1,23 @@
 //1. importar express
 const express = require('express');
+const geneticDiseases = require('./genetic-diseases/genetic-diseases.route')
 
 //2. crearnos una constante app que tendra
 //todas las funcionalidades de express
 const app = express();
 
+const calculateRequestTime = (req, res, next) => {
+  const requestTime = new Date().toISOString();
+
+  req.requestTime = requestTime;
+  next()
+}
+
 app.use(express.json()); //permitir poder obtener el body en formato json
 app.use(express.urlencoded({ extended: true })); //permitir poder obtener el body en formato url-encoded
+app.use(calculateRequestTime)
 
-//definicion de funciones
-const findAll = (req, res) => {
-
-  return res.status(200).json({
-    message: 'method get - findAll'
-  })
-}
-
-const create = (req, res) => {
-
-  const disease = req.body;
-
-  return res.status(201).json({
-    message: 'method post - create',
-    data: disease
-  })
-}
-
-//3. definir el endpoint
-//endpoint para buscar todas las enfermedades geneticas
-app.get('/api/v1/genetic-diseases', findAll)
-
-//endpoint para crear una enfermedad genetica
-app.post('/api/v1/genetic-diseases', create)
-
-//endpoint para buscar UNA enfermedad gentica
-app.get('/api/v1/genetic-diseases/:id', (req, res) => {
-
-  console.log(req.params)
-
-  return res.status(200).json({
-    message: 'method get - findOne',
-    id: req.params.id
-  })
-})
-
-//endpoint para actualizar UNA enfermedad genetica
-app.patch('/api/v1/genetic-diseases/:id', (req, res) => {
-  const { id } = req.params;
-
-  return res.status(200).json({
-    message: 'method patch - update',
-    id,
-  })
-})
-
-app.delete('/api/v1/genetic-diseases/:id', (req, res) => {
-  const { id } = req.params;
-
-  return res.status(200).json({
-    message: 'method delete - delete',
-    id
-  })
-})
-
+app.use('/api/v1', geneticDiseases)
 
 //4.poner a escuchar el servidor por un puerto
 app.listen(3000, () => {
